3b812c51ddaea605984efe4a93cd145deb790af1
Adir Noyman <45500652+AdirNoyman@users.noreply.github.com>
2019-11-20 20:59:10 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { SelfService } from './SelfService';
+
+function App() {
+  return (
+    <div className="App">
+      <SelfService />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/SelfService.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const SelfService = () => {
+    return (
+        <div>
+            <h1>Adiros The Great!</h1>
+        </div>
+    )
+}
