0624e15975cfdf2fe8f936d827ce14a1a03dfef9
Adir Noyman <45500652+AdirNoyman@users.noreply.github.com>
2019-12-16 20:09:43 +0200

    Add files via upload

---
+++ build/precache-manifest.2494441cfb81674b9a19a59cf6747525.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdd13824a65f63496b1f4f15c7c76921",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b210bb1b7ff39c802240",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "da6d83c8883ee201fa48",
+    "url": "/static/css/main.97e43a9b.chunk.css"
+  },
+  {
+    "revision": "b210bb1b7ff39c802240",
+    "url": "/static/js/2.1facb6bd.chunk.js"
+  },
+  {
+    "revision": "da6d83c8883ee201fa48",
+    "url": "/static/js/main.f5992d5d.chunk.js"
+  },
+  {
+    "revision": "8208594584c2fa53bca3",
+    "url": "/static/js/runtime-main.39473082.js"
+  }
+]);
