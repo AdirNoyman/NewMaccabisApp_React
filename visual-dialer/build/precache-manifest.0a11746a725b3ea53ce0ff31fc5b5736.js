89d7e847e9dea883a8611c8ca707957052f77ce1
Adir Noyman <45500652+AdirNoyman@users.noreply.github.com>
2020-01-29 19:29:23 +0200

    Add files via upload

---
+++ visual-dialer/build/precache-manifest.0a11746a725b3ea53ce0ff31fc5b5736.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f2e843611b43edb29145d2600554c71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "664a7aaf417d17744920",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "b09b420aec59bac3c018",
+    "url": "/static/css/main.97e43a9b.chunk.css"
+  },
+  {
+    "revision": "664a7aaf417d17744920",
+    "url": "/static/js/2.d50a0597.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.d50a0597.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b09b420aec59bac3c018",
+    "url": "/static/js/main.ef70e1a6.chunk.js"
+  },
+  {
+    "revision": "59ce43e6c3dcb74340c8",
+    "url": "/static/js/runtime-main.5ee73913.js"
+  }
+]);
